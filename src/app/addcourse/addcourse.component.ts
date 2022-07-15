4d7bff7d123e1bf885d47d12f2bcb0af18322712
Akhil7654 <msdcr88@gmail.com>
2022-07-15 14:39:51 +0530

    router added

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-addcourse',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addcourse.component.css']
 })
 export class AddcourseComponent implements OnInit {
-
-  constructor() { }
+  course={courseTitle:"",
+  courseDescription:"",
+  courseDate:"",
+  courseDuration:"",
+  courseVenue:""
+  }
+  constructor(private api:ApiService) { }
 
   ngOnInit(): void {
   }
-
+  Addcourse(){
+    this.api.addcourse(this.course)
+    .subscribe((data)=>{
+    
+    alert("Success");
+    
+  })
+  }
 }
--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -12,4 +12,7 @@ export class ApiService {
   viewcourse=()=>{
     return this.http.get("https://mylinkurcodesapp.herokuapp.com/getcourses")
   }
+  addcourse(course:any){
+    return this.http.post<any>("https://mylinkurcodesapp.herokuapp.com/addcourse",course)
+  }
 }
